463a95faa05c1f57914e24228348f7fc19a3120a
Kharamot <kameronharamoto@Kamerons-Mac.local>
2017-01-12 19:26:37 -0800

    .
    .

--- Joker1/ViewController.swift
+++ Joker1/ViewController.swift
@@ -20,6 +20,11 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func answerTapped(_ sender: Any) {
+        punchLine.isHidden = !punchLine.isHidden
+    }
 
+    @IBOutlet weak var answerOutlet: UIButton!
+    @IBOutlet weak var punchLine: UILabel!
 }
 
